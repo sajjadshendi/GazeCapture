7bdb9079db90c459c881406a6aa56f055d0fe1a1
sajjadshendi <60941907+sajjadshendi@users.noreply.github.com>
2023-05-26 11:47:46 +0330

    Update ITracker_Prediction_Data.py

--- pytorch/ITracker_Prediction_Data.py
+++ pytorch/ITracker_Prediction_Data.py
@@ -1,1 +1,29 @@
-
+import cv2
+class ITracker_Prediction_Data():
+  
+    def __init__(self, dataPath, imSize = (224, 224)):
+      self.dataPath = dataPath
+      self.imSize = imSize
+    
+    def FrameCapture(self):
+  
+      # Path to video file
+      vidObj = cv2.VideoCapture(self.datapath)
+  
+      # Used as counter variable
+      count = 0
+  
+      # checks whether frames were extracted
+      success = 1
+  
+      while success:
+  
+          # vidObj object calls read
+          # function extract frames
+          success, image = vidObj.read()
+  
+          # Saves the frames with frame-count
+          cv2.imwrite("frame%d.jpg" % count, image)
+  
+          count += 1
+    
